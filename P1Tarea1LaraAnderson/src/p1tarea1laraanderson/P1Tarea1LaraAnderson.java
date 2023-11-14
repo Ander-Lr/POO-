19ed121fed97e674f7cc880ca7a85004ea623d59
Anderson Lara <150303508+Ander-Lr@users.noreply.github.com>
2023-11-13 20:32:47 -0500

    Add files via upload

---
+++ P1Tarea1LaraAnderson/src/p1tarea1laraanderson/P1Tarea1LaraAnderson.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p1tarea1laraanderson;
+
+/**
+ *
+ * @author Usuario
+ */
+public class P1Tarea1LaraAnderson {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE");
+        System.out.println("PROGRAMACION ORIENTADA A OBJETOS");
+        System.out.println("SEGUNDO ITIN");
+        System.out.println("ING. VERONICA MARTINEZ");
+        System.out.println("CALCULADORA BÁSICA");
+        
+    }
+    
+}
